b01caa613ac21582a166382f9f5f72da10414574
sunabove <sunabove@nate.com>
2019-08-27 05:50:32 +0000

    **

--- camera_stream.py
+++ camera_stream.py
@@ -15,8 +15,7 @@ PAGE="""\
 <title>Raspberry Pi - Surveillance Camera</title>
 </head>
 <body>
-<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
+<center><img src="stream.mjpg" width="" height=""></center>
 </body>
 </html>
 """
