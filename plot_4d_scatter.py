6ba2edf23e80a49f00e145e76bbaab4feb44c731
sunabove <sunabove@users.noreply.github.com>
2019-09-06 23:28:25 +0900

    plot 4d scatter data example

---
+++ plot_4d_scatter.py
@@ -0,0 +1,20 @@
+# plot 4d scatter
+
+import numpy as np
+from scipy import stats
+from mayavi import mlab
+
+mu, sigma = 0, 0.1 
+x = 10*np.random.normal(mu, sigma, 5000)
+y = 10*np.random.normal(mu, sigma, 5000)
+z = 10*np.random.normal(mu, sigma, 5000)
+
+xyz = np.vstack([x,y,z])
+kde = stats.gaussian_kde(xyz)
+density = kde(xyz)
+
+# Plot scatter with mayavi
+figure = mlab.figure('DensityPlot')
+pts = mlab.points3d(x, y, z, density, scale_mode='none', scale_factor=0.07)
+mlab.axes()
+mlab.show()
