ca6dc1a9170e08279d591df642fc7e8495e7efdd
sunabove <sunabove@users.noreply.github.com>
2019-09-26 20:41:48 +0900

    **

--- MagneticDensityPlotter.py
+++ MagneticDensityPlotter.py
@@ -7,7 +7,7 @@ from mayavi import mlab
 
 import pandas as pd
 
-df = pd.read_excel ( r'data.xlsx')
+df = pd.read_excel ( r'magnetic_density_data.xlsx')
 #(use "r" before the path string to address special character, such as '\').
 # Don't forget to put the file name at the end of the path + '.xlsx'
 
